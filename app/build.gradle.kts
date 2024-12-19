2a2c152b81ba80b187dffbb8fcfdc2a1b73e0134
EhsanTgv <EhsanTgv@gmail.com>
2024-12-19 11:32:41 +0300

    Implement Circular Reveal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.taghavi.circularreveal"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.taghavi.circularreveal"
